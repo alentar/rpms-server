5b6c13d24dd519ea8cd05f7ed5122bd1652097da
Kasun Vithanage <alankasun@gmail.com>
2018-03-26 14:29:06 +0530

    Added wards controller
    Signed-off-by: Kasun Vithanage <alankasun@gmail.com>

--- src/controllers/ward.controller.js
+++ src/controllers/ward.controller.js
@@ -4,27 +4,11 @@ const Ward = require('../models/ward.model')
 const httpStatus = require('http-status')
 const APIError = require('../utils/APIError')
 const ObjectId = require('mongoose').Types.ObjectId
+const _ = require('lodash')
 
 exports.create = async (req, res, next) => {
   try {
-    const ward = new Ward(req.body)
-
-    if (req.body.beds) {
-      const err = new APIError('Invalid ObjectId')
-      err.errors = []
-      let hasError = false
-      req.body.beds.forEach(bed => {
-        if (bed.deviceID !== undefined && bed.deviceID !== null && bed.deviceID !== '' && !ObjectId.isValid(bed.deviceID)) {
-          hasError = true
-          err.errors.push({
-            field: bed,
-            error: 'Invalid ObjectId'
-          })
-        }
-      })
-
-      if (hasError) throw err
-    }
+    const ward = new Ward(_.omit(req.body, ['beds']))
 
     const savedWard = await ward.save()
     res.status(httpStatus.CREATED)
@@ -50,10 +34,34 @@ exports.delete = async (req, res, next) => {
   }
 }
 
-exports.addBeds = async (req, res, next) => {
+exports.update = async (req, res, next) => {
   try {
+    if (!ObjectId.isValid(req.params.wardId)) throw new APIError('Requested resource not found', httpStatus.NOT_FOUND)
 
-  } catch (err) {
-    return next(err)
+    const wardObject = _.omit(req.body, ['beds'])
+    const ward = await Ward.findByIdAndUpdate(req.params.wardId, wardObject, { new: true })
+    return res.json({ ward })
+  } catch (error) {
+    return next(Ward.checkDuplicateWardError(error))
+  }
+}
+
+exports.view = async (req, res, next) => {
+  try {
+    if (!ObjectId.isValid(req.params.wardId)) throw new APIError('Requested resource not found', httpStatus.NOT_FOUND)
+
+    const ward = await Ward.findById(req.params.wardId)
+    return res.json({ ward })
+  } catch (error) {
+    return next(error)
+  }
+}
+
+exports.index = async (req, res, next) => {
+  try {
+    const result = await Ward.list(req.query)
+    return res.json(result)
+  } catch (error) {
+    return next(error)
   }
 }
