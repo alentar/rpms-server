2336d3f76e1afedbd866174a9ee2869d5a7eee63
Kasun Vithanage <alankasun@gmail.com>
2018-03-26 14:29:36 +0530

    ward model added
    Signed-off-by: Kasun Vithanage <alankasun@gmail.com>

--- src/models/ward.model.js
+++ src/models/ward.model.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose')
 const BedSchema = require('./bed.model').BedSchema
 const httpStatus = require('http-status')
+const APIError = require('../utils/APIError')
 
 const wardSchema = new mongoose.Schema({
   name: {
@@ -14,7 +15,8 @@ const wardSchema = new mongoose.Schema({
     type: Number,
     required: true,
     unique: true,
-    index: true
+    index: true,
+    sparse: true
   },
 
   beds: [
@@ -25,7 +27,7 @@ const wardSchema = new mongoose.Schema({
 })
 
 wardSchema.statics = {
-  checkDuplicateWardeError (err) {
+  checkDuplicateWardError (err) {
     if (err.code === 11000) {
       var error = new Error('Ward number already exists')
       error.errors = [{
@@ -38,6 +40,30 @@ wardSchema.statics = {
     }
 
     return err
+  },
+
+  async list ({page = 1, perPage = 30}) {
+    page = Number(page)
+    perPage = Number(perPage)
+
+    if (!page || page <= 0) throw new APIError('Invalid page')
+    if (!perPage || (perPage <= 0 && perPage !== -1)) throw new APIError('Invalid perPage')
+
+    let results = null
+    if (perPage === -1) {
+      results = await Ward.find().sort({'createdAt': -1})
+    } else {
+      results = await Ward.find()
+        .limit(perPage)
+        .skip(perPage * (page - 1))
+        .sort({'createdAt': -1})
+    }
+
+    const wards = results
+    const total = await Ward.count()
+    const pages = Math.ceil(total / perPage)
+
+    return {wards, pages, page, perPage, total}
   }
 }
 
