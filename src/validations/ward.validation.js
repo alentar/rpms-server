440d83ad4d31de044af0ca379c769ebeab4a6e26
Kasun Vithanage <alankasun@gmail.com>
2018-03-26 14:34:52 +0530

    ward validation
    Signed-off-by: Kasun Vithanage <alankasun@gmail.com>

--- src/validations/ward.validation.js
+++ src/validations/ward.validation.js
@@ -6,11 +6,14 @@ module.exports = {
   create: {
     body: {
       name: Joi.string().max(100).optional(),
-      number: Joi.number().required().positive(),
-      beds: Joi.array().unique((a, b) => { return a.number === b.number }).items(Joi.object().keys({
-        number: Joi.number(),
-        deviceID: Joi.string().optional()
-      })).optional()
+      number: Joi.number().required().min(1)
+    }
+  },
+
+  update: {
+    body: {
+      name: Joi.string().max(100).optional(),
+      number: Joi.number().optional().positive()
     }
   }
 }
