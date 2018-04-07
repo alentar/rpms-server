1cce670e7b2ad0cd866112770fbcb8edd7f310fe
Kasun Vithanage <alankasun@gmail.com>
2018-04-07 13:40:01 +0530

    mark as read

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const User = require('../models/user.model')
+const Notfication = require('../models/notification.model')
 const httpStatus = require('http-status')
 const APIError = require('../utils/APIError')
 const ObjectId = require('mongoose').Types.ObjectId
@@ -10,7 +11,10 @@ const bcrypt = require('bcrypt-nodejs')
 exports.create = async (req, res, next) => {
   try {
     if (req.body.name) {
-      req.body.name = req.body.name.split(' ').map((word) => { return word[0].toUpperCase() + word.substr(1).toLowerCase() }).join(' ')
+      req.body.name = req.body.name
+        .match(/\S+/g).map((word) => {
+          return word[0].toUpperCase() + word.substr(1).toLowerCase()
+        }).join(' ')
     }
     if (!req.body.password) req.body.password = req.body.nic
     const user = new User(req.body)
@@ -66,7 +70,10 @@ exports.update = async (req, res, next) => {
     }
 
     if (userObj.name) {
-      userObj.name = userObj.name.split(' ').map((word) => { return word[0].toUpperCase() + word.substr(1).toLowerCase() }).join(' ')
+      userObj.name = userObj.name
+        .match(/\S+/g).map((word) => {
+          return word[0].toUpperCase() + word.substr(1).toLowerCase()
+        }).join(' ')
     }
 
     const user = await User.findByIdAndUpdate(req.params.userID, userObj, {new: true})
@@ -92,6 +99,46 @@ exports.me = async (req, res, next) => {
       .select('-password')
 
     return res.json({ user })
+  } catch (error) {
+    return next(error)
+  }
+}
+
+exports.myNotifications = async (req, res, next) => {
+  try {
+    const userId = req.user._id
+
+    const notifications = await User
+      .findById(userId)
+      .populate({path: 'notifications', options: { sort: { createdAt: -1 } }})
+      .select('notifications')
+
+    return res.json(notifications)
+  } catch (error) {
+    return next(error)
+  }
+}
+
+exports.markNotificationAsRead = async (req, res, next) => {
+  try {
+    const userId = req.user._id
+    console.log(req.user)
+
+    const notficationId = req.params.notificationId
+    if (!ObjectId.isValid(notficationId)) throw new APIError('Invalid notification', httpStatus.NOT_FOUND)
+
+    // check if user has this notification
+    const user = await User.findOne({ _id: ObjectId(userId), notifications: ObjectId(notficationId) })
+
+    if (!user) throw new APIError('Resource not avaliable', httpStatus.UNAUTHORIZED)
+
+    if (!req.query.read || !['true', 'false'].includes(req.query.read)) {
+      req.query.read = 'true'
+    }
+
+    const result = await Notfication.findByIdAndUpdate(notficationId, { read: req.query.read === 'true' }, { new: true })
+    if (result === null) throw new APIError('Something went wrong')
+    return res.json({ _id: result._id, read: result.read })
   } catch (error) {
     return next(error)
   }
