dc95b141e9a27c2a276c9c30aa566dce018e2336
Kasun Vithanage <alankasun@gmail.com>
2018-05-17 20:41:37 +0530

    records

--- src/models/patient.model.js
+++ src/models/patient.model.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose')
 const httpStatus = require('http-status')
 const APIError = require('../utils/APIError')
 
+const recordSchema = new mongoose.Schema({
+  value: {
+    type: Number
+  }
+}, { timestamps: true })
+
 const patientSchema = new mongoose.Schema({
   bht: {
     type: String,
@@ -123,13 +129,8 @@ const patientSchema = new mongoose.Schema({
   },
 
   records: {
-    temperature: [{
-      type: Number
-    }],
-
-    bpm: [{
-      type: Number
-    }]
+    temperature: [recordSchema],
+    bpm: [recordSchema]
   },
 
   ward: {
